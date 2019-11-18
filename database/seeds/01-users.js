77ee97ecfdaf97d36d899b9048440b362ab8fbfc
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-18 10:45:09 +0100

    :art: Re-arrange code

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,13 +1,31 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
+  return knex("users")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('users').insert([
-        { first_name: 'Maaruf', last_name: ' Dauda', email: 'emkay', password: 'aLongPassword', phone:'aPhone'},
-        { first_name: 'Maaruf', last_name: ' Dauda', email: 'emkay1', password: 'aLongPassword', phone:'aPhone'},
-        { first_name: 'Maaruf', last_name: ' Dauda', email: 'emkay2', password: 'aLongPassword', phone:'aPhone'}
+      return knex("users").insert([
+        {
+          first_name: "Maaruf",
+          last_name: " Dauda",
+          email: "emkay",
+          password: "aLongPassword",
+          phone: "aPhone"
+        },
+        {
+          first_name: "Maaruf",
+          last_name: " Dauda",
+          email: "emkay1",
+          password: "aLongPassword",
+          phone: "aPhone"
+        },
+        {
+          first_name: "Maaruf",
+          last_name: " Dauda",
+          email: "emkay2",
+          password: "aLongPassword",
+          phone: "aPhone"
+        }
       ]);
     });
 };

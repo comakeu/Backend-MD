69d6f890e6062b40eca65f1a13a23255b32997f9
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-18 15:43:53 +0100

    :bug: Remove raw sql [didn't work]

--- database/seeds/00-cleanup.js
+++ database/seeds/00-cleanup.js
@@ -1,10 +1,7 @@
 const cleaner = require('knex-cleaner');
 
 exports.seed = async function(knex) {
-  await knex.raw('SET foreign_key_checks = 0');
-  await cleaner.clean(knex, {
+  return cleaner.clean(knex, {
     ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
   });
-  await knex.raw('SET foreign_key_checks = 1');
-
 };

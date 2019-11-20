35eb907b369bd946ad97989411ee11419889ad02
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-20 11:36:32 +0100

    :sparkles: Add endpoint to delete vote

--- issues/issuesModel.js
+++ issues/issuesModel.js
@@ -1,8 +1,48 @@
 const db = require("../database/dbConfig");
-const votesModel = require('../votes/votesModel')
+const votesModel = require("../votes/votesModel");
 
 function get(id) {
-  let query = db("issues as i");
+  let query = db("issues as i")
+    .select(
+      "i.issue_id",
+      "i.description",
+      "i.longitude",
+      "i.latitude",
+      "i.user_id",
+      "u.user_id",
+      "u.first_name",
+      "u.last_name"
+    )
+    .join("users as u", "i.user_id", "u.user_id");
+
+
+    /**
+     * Alternate knex query to return votes and removes the need to use a second promise:
+     * 
+     *  let query = db("issues as i")
+    .select(
+      "i.issue_id",
+      "i.description",
+      "i.longitude",
+      "i.latitude",
+      "i.user_id",
+      "u.user_id",
+      "u.first_name",
+      "u.last_name"
+    )
+    .count("v.issue_id", { as: "votes" })
+    .join("users as u", "i.user_id", "u.user_id")
+    .leftJoin("votes as v", "v.issue_id", "i.issue_id")
+    .groupBy(
+      "i.issue_id",
+      "i.description",
+      "i.latitude",
+      "i.longitude",
+      "u.first_name",
+      "u.last_name"
+    )
+    .orderBy("votes", "desc");
+     */
 
   if (id) query.where("i.issue_id", id).first();
 
--- votes/votesRouter.js
+++ votes/votesRouter.js
@@ -1,34 +1,55 @@
 const votes = require("express").Router();
-const db = require("./votesModel.js");
+const votesModel = require("./votesModel.js");
 const restricted = require("../utils/restrictedMiddleware");
 
 votes.post("/", restricted, (req, res) => {
   const user_id = req.decodedToken.subject;
   const issue_id = req.body.issue_id;
 
-  db.createVote(user_id, issue_id)
+  votesModel
+    .createVote(user_id, issue_id)
     .then(() => {
-        db.getCount({ issue_id }).then(votes => {
-            votes.issue_id = issue_id;
-            res
-              .status(201)
-              .json({ error: false, message: "Vote successful", data: votes });
-          });
+      votesModel.getCount({ issue_id }).then(votes => {
+        votes.issue_id = issue_id;
+        res
+          .status(201)
+          .json({ error: false, message: "Vote successful", data: votes });
+      });
     })
     .catch(err => {
       if (err.errno === 19) {
-        res
-          .status(500)
-          .json({
-            error: true,
-            message: `Failed. User ${user_id} might have already voted for this issue`
-          });
+        res.status(500).json({
+          error: true,
+          message: `Failed. User ${user_id} might have already voted for this issue`
+        });
       } else {
         res
           .status(500)
-          .json({ error: true, message: "Failed to vote", data: err });
+          .json({ error: true, message: `Failed to vote: ${err.message}`, data: err });
       }
     });
+});
+
+votes.delete("/:issue_id", restricted, (req, res) => {
+  const user_id = req.decodedToken.subject;
+  const issue_id = req.params.issue_id;
+  votesModel
+    .removeVote(user_id, issue_id)
+    .then(flag => {
+      if (flag) {
+        res.status(200).json({ error: false, message: "Vote removed" });
+      } else {
+        res.status(404).json({
+          error: true,
+          message: `User ${user_id} has not voted for this issue {issue_id: ${issue_id}} yet`
+        });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: true, message: "Failed to vote", data: err })
+    );
 });
 
 module.exports = votes;
